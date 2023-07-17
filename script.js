e127c3d5da004829ac8481669b2abb27b9ee7155
ZarathustraSama <mr.gabriel@live.it>
2023-07-17 07:31:47 +0200

    Added basic operations and variables

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+const operators = ['+', '-', '*', '/'];
