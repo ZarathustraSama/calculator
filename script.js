40d3da27c6744c19fbcf587028e64f6645352ffd
ZarathustraSama <mr.gabriel@live.it>
2023-07-18 10:56:51 +0200

    Cleaned code + fixed backtrack function behaviour

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Global variables
+let firstNumber = null;
+let secondNumber = null;
+let result = null;
+let operator = null;
+let decimal = false;
+let displayValue = '';
+const operatorsArray = ['+', '-', '*', '/'];
+const keyboardNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// DOM variables
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('#clear');
+const decimalButton = document.querySelector('#decimal');
+const backspaceButton = document.querySelector('#backspace');
+
+
+// Event listeners
+numbers.forEach(number => {number.addEventListener('click', () => updateDisplayValue(number.id));});
+operators.forEach(symbol => {symbol.addEventListener('click', () => decideOperation(symbol.id));});
+clearButton.addEventListener('click', clearCalculator);
+decimalButton.addEventListener('click', enableDecimals);
+backspaceButton.addEventListener('click', backTrack);
+
+function updateDisplayValue(value) {
+    if (displayValue.length <= 6) {
+        displayValue += value;
+        display.textContent = displayValue;
+    };
+}
+
 function add(number1, number2) {
     return number1 + number2;
 }
@@ -15,13 +48,6 @@ function divide(number1, number2) {
     return +(number1 / number2).toFixed(6);
 }
 
-let firstNumber = null;
-let secondNumber = null;
-let result = null;
-const operatorsArray = ['+', '-', '*', '/'];
-let operator = null;
-const keyboardNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 function operate(firstNumber, operator, secondNumber) {
     if (operator === '+') return add(firstNumber, secondNumber);
     else if (operator === '-') return subtract(firstNumber, secondNumber);
@@ -29,28 +55,6 @@ function operate(firstNumber, operator, secondNumber) {
     else if (operator === '/') return divide(firstNumber, secondNumber);
     else return;          
 }
-
-let displayValue = '';
-
-const display = document.querySelector('.display');
-const numbers = document.querySelectorAll('.number');
-
-numbers.forEach(number => {
-    number.addEventListener('click', () => updateDisplayValue(number.id));
-});
-
-function updateDisplayValue(value) {
-    if (displayValue.length <= 6) {
-        displayValue += value;
-        display.textContent = displayValue;
-    };
-}
-
-const operators = document.querySelectorAll('.operator');
-
-operators.forEach(symbol => {
-    symbol.addEventListener('click', () => decideOperation(symbol.id))
-})
 
 function decideOperation(nextOperator) {
     if (displayValue !== '') {
@@ -78,9 +82,6 @@ function decideOperation(nextOperator) {
     }
 }
 
-const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clearCalculator);
-
 function clearCalculator() {
     displayValue = '';
     display.textContent = '0';
@@ -91,10 +92,6 @@ function clearCalculator() {
     decimal = false;
 }
 
-let decimal = false;
-const decimalButton = document.querySelector('#decimal');
-decimalButton.addEventListener('click', enableDecimals);
-
 function enableDecimals() {
     if (!decimal) {
         decimal = true;
@@ -102,13 +99,11 @@ function enableDecimals() {
     }
 }
 
-const backspaceButton = document.querySelector('#backspace');
-backspaceButton.addEventListener('click', backTrack);
-
 function backTrack() {
     if (displayValue.length > 1) {
+        if (displayValue[displayValue.length - 1] === '.') decimal = false;
         displayValue = displayValue.replace(/.$/, '');
-        updateDisplayValue(displayValue);
+        display.textContent = displayValue;
     }
     else if (displayValue.length === 1) {
         displayValue = '';
